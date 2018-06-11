bff21f06e1561ad370692677acfa06e834a54cdd
ivaylen <ivaylen@abv.bg>
2018-06-11 22:28:57 +0300

    creating-posts with details component home and list

--- src/app/createpost/createpost.module.ts
+++ src/app/createpost/createpost.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostHomeComponent } from './post-home/post-home.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PostListComponent, PostDetailComponent, PostHomeComponent]
 })
 export class CreatepostModule { }
