783dbe47719f7974585a5752b0572867dbf2a8b8
ivaylen <ivaylen@abv.bg>
2018-06-12 21:40:53 +0300

    environment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9U3XqxccvRVkKL3jgJpDaE0zwkpMWGEA",
+    authDomain: "angularprojectblog.firebaseapp.com",
+    databaseURL: "https://angularprojectblog.firebaseio.com",
+    projectId: "angularprojectblog",
+    storageBucket: "angularprojectblog.appspot.com",
+    messagingSenderId: "12065606370"
+  }
 };
